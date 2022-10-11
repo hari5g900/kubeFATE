fd6a07c21f4cd95a7ec25f561a40f5a1c2dfc5de
Hari Sankar Madhukumar <91274654+hari5g900@users.noreply.github.com>
2022-10-11 10:49:21 +0100

    Add files via upload

---
+++ data_collect.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Script start time
+START=$(date +%s)
+
+## Total run time
+DURRATION=$((60 * 60 * 24))
+
+## Total running time
+UPTIME=$(($(date +%s) - $START))
+
+while [[ $UPTIME < $DURRATION ]]; do
+
+    ## Logic here...
+    echo -n "Time remaining: "
+    echo $(($DURRATION - $UPTIME))
+    echo ",$(date)" >> data.csv
+    curl http://192.168.100.1/index.cgi?page=modemStatusData | awk -F'##' '/images/{print "," $15 "," $12}' >> data.csv
+    ./librespeed-cli --duration 10 --csv --server 51 | awk -F "," '{print "," $5 "," $6 "," $7 "," $7}' >> data.csv
+
+    ## Sleep for a bit
+    sleep 300
+
+    ## Update running time
+    UPTIME=$(($(date +%s) - $START))
+
+done
